bfa7339bbf097428e324ce868e006ea83b0c6ac1
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-19 17:04:12 -0300

    Add models index file for Student and Courses
    Introduced src/models/index.ts to centralize exports for Student and Courses models. This allows for easier imports and management of model references throughout the project.

---
+++ src/models/index.ts
@@ -0,0 +1,6 @@
+import { Student } from "src/students/students.model";
+import { Courses } from "src/courses/courses.model";
+
+export const models = [Student, Courses];
+
+export { Student, Courses };
