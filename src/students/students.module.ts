2ea10ec658e7d306e625b0268ac55a2781eed10c
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-18 21:28:00 -0300

    Change import quotes to double quotes in students.module.ts
    Updated all import statements in students.module.ts to use double quotes instead of single quotes for consistency with code style.

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { StudentsController } from './students.controller';
-import { StudentsService } from './students.service';
-import { StudentsRepository } from './students.repository';
-import { Student } from './students.model';
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { StudentsController } from "./students.controller";
+import { StudentsService } from "./students.service";
+import { StudentsRepository } from "./students.repository";
+import { Student } from "./students.model";
 
 @Module({
   imports: [SequelizeModule.forFeature([Student])],
