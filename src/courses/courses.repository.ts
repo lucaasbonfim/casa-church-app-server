08e2afca4a529afe3c80ffece73f8e6b799b0aaf
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-18 21:27:45 -0300

    Add CoursesRepository with CRUD methods
    Introduces a CoursesRepository class for managing Courses entities using Sequelize. Provides methods for finding, creating, updating, replacing, and deleting course records.

---
+++ src/courses/courses.repository.ts
@@ -0,0 +1,45 @@
+import { InjectModel } from "@nestjs/sequelize";
+import { Courses } from "./courses.model";
+
+export class CoursesRepository {
+  constructor(
+    @InjectModel(Courses)
+    private readonly coursesModel: typeof Courses
+  ) {}
+
+  async findAll(): Promise<Courses[] | null> {
+    const courses = await this.coursesModel.findAll();
+    return courses;
+  }
+
+  async getById(id: string): Promise<Courses | null> {
+    const course = await this.coursesModel.findByPk(id);
+    return course;
+  }
+
+  async create(course: Courses): Promise<Courses> {
+    const newCourse = await this.coursesModel.create(course);
+
+    return newCourse;
+  }
+
+  async replace(id: string, newData: Courses): Promise<Courses> {
+    const course = await this.getById(id);
+
+    return await course!.update(newData);
+  }
+
+  async update(id: string, newData: Partial<Courses>): Promise<Courses> {
+    const course = await this.getById(id);
+
+    return await course!.update(newData);
+  }
+
+  async delete(id: string): Promise<null> {
+    const course = await this.getById(id);
+
+    await course!.destroy();
+
+    return null;
+  }
+}
