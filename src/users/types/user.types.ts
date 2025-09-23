a493402a4c00957b7b7919d1a40d3c1c3e1660d9
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-22 22:48:52 -0300

    feat: creating CreateUser and UpdateUser in user.types.ts

--- src/users/types/user.types.ts
+++ src/users/types/user.types.ts
@@ -9,6 +9,22 @@ export type User = {
   updatedAt: Date;
 };
 
+export type CreateUser = {
+  name: string;
+  email: string;
+  password: string;
+  role: UserRoles;
+  // active?: boolean;
+};
+
+export type UpdateUser = {
+  name?: string;
+  email?: string;
+  password?: string;
+  role?: UserRoles;
+  active?: boolean;
+};
+
 export enum UserRoles {
   USER = "user",
   ADMIN = "admin",
