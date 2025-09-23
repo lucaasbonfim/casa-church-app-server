e123b5308007fcc8852e864f1ecf4aae93bc06b7
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-22 22:47:57 -0300

    feat: importing AuthModule in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { StudentsModule } from "src/students/students.module";
 import { CoursesModule } from "src/courses/courses.module";
+import { AuthModule } from "src/auth/auth.module";
 import { DatabaseModule } from "src/config/database.module";
 import { UsersModule } from "src/users/users.module";
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
