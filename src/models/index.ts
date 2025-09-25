8b2856f71154242ce8db987d6b087b64113bc1de
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-25 18:51:50 -0300

    feat: import event model in models central index

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,7 +2,7 @@ import { Student } from "src/students/entities/students.model";
 import { Courses } from "src/courses/entities/courses.model";
 import { StudentCourses } from "src/students/entities/studentCourses.model";
 import { User } from "src/users/entities/user.model";
+import { Event } from "src/events/entities/event.model";
+export const models = [Student, Courses, StudentCourses, User, Event];
 
-export const models = [Student, Courses, StudentCourses, User];
-
-export { Student, Courses, StudentCourses, User };
+export { Student, Courses, StudentCourses, User, Event };
