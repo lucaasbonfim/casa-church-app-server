fef980a6dff088e45f48954b81e65cde216143d7
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-25 20:34:23 -0300

    feat: importing AuthModule in EventsModule

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -3,10 +3,11 @@ import { SequelizeModule } from "@nestjs/sequelize";
 import { EventsService } from "./events.service";
 import { EventsController } from "./events.controller";
 import { EventsRepository } from "./events.repository";
+import { AuthModule } from "src/auth/auth.module";
 import { models } from "src/models";
 
 @Module({
-  imports: [SequelizeModule.forFeature(models)],
+  imports: [SequelizeModule.forFeature(models), AuthModule],
   controllers: [EventsController],
   providers: [EventsService, EventsRepository],
 })
