68845ab1086ff768da3e259b7bd8f52d915cd374
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-10-13 23:00:47 -0300

    wip: implementing ApiProperty to all dtos in the plataform

--- src/registrations/dto/update-registration.dto.ts
+++ src/registrations/dto/update-registration.dto.ts
@@ -1,6 +1,11 @@
 import { IsString, IsNotEmpty } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class UpdateRegistrationDto {
+  @ApiProperty({
+    example: "confirmed",
+    description: "Novo status da inscrição (ex: confirmed, pending, canceled)",
+  })
   @IsString({ message: "O status da inscrição deve ser do tipo string" })
   @IsNotEmpty({ message: "O status da inscrição não pode ser vazio" })
   status: string;
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,13 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateUserDto } from "./create-user.dto";
 import { IsBoolean, IsOptional } from "class-validator";
+import { ApiPropertyOptional } from "@nestjs/swagger";
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiPropertyOptional({
+    example: true,
+    description: "Define se o usuário está ativo ou inativo",
+  })
   @IsOptional()
   @IsBoolean({ message: "O campo ativo deve ser um booleano." })
   active?: boolean;
