215a468be27902f5ba2033c5bd5f451f53c1e757
lian mendes <nailptm@gmail.com>
2025-10-16 20:28:54 -0300

    feat: implementing cache to every system get routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "src/auth/auth.module";
 import { DatabaseModule } from "src/config/database.module";
+import { DataCacheModule } from "src/config/data-cache.module";
 import { UsersModule } from "src/users/users.module";
 import { EventsModule } from "src/events/events.module";
 import { RegistrationsModule } from "src/registrations/registrations.module";
@@ -13,6 +14,7 @@ import { DonationsModule } from "src/donations/donations.module";
 @Module({
   imports: [
     DatabaseModule,
+    DataCacheModule,
     AuthModule,
     UsersModule,
     EventsModule,
