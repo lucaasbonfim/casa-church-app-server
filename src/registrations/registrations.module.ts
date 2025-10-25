927505525749921b91677fd1433741811ca33bc4
Lucas <lucascardoso2103@gmail.com>
2025-10-24 21:24:20 -0300

    chore(registrations): import ScheduleModule and AutoCancelTask

--- src/registrations/registrations.module.ts
+++ src/registrations/registrations.module.ts
@@ -1,14 +1,20 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { ScheduleModule } from "@nestjs/schedule";
 import { RegistrationsService } from "./registrations.service";
 import { RegistrationsController } from "./registrations.controller";
 import { RegistrationsRepotisory } from "./registrations.repository";
 import { AuthModule } from "src/auth/auth.module";
 import { models } from "src/models";
+import { AutoCancelTask } from "./tasks/auto-cancel.task";
 
 @Module({
-  imports: [SequelizeModule.forFeature(models), AuthModule],
+  imports: [
+    SequelizeModule.forFeature(models),
+    AuthModule,
+    ScheduleModule.forRoot(),
+  ],
   controllers: [RegistrationsController],
-  providers: [RegistrationsService, RegistrationsRepotisory],
+  providers: [RegistrationsService, RegistrationsRepotisory, AutoCancelTask],
 })
 export class RegistrationsModule {}
